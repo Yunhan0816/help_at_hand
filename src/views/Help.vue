248fb2926fe385f1e496ecb2680e7c7a4763fd69
Ishika <jishika@bu.edu>
2020-02-01 16:31:00 -0500

    background image

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,12 +1,7 @@
 <template>
 
   <v-app id="inspire">
-   
-    <!-- <div id="app"> -->
-      
-      <v-parallax src="https://saludalmaximo.com/wp-content/uploads/2019/08/11263-Sexual_Assault_Resource_Page-1296x728-Header-1024x576.jpg" height = "700">
-        <v-row align="center" justify="center">
-           <v-app-bar app clipped>
+    <v-app-bar app clipped>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title>Help At Hand</v-toolbar-title>
       <v-spacer></v-spacer>
@@ -14,8 +9,12 @@
         <v-icon>mdi-doctor</v-icon>
       </v-btn> -->
     </v-app-bar>
+    <div id="app">
+      
+      <v-parallax src="https://saludalmaximo.com/wp-content/uploads/2019/08/11263-Sexual_Assault_Resource_Page-1296x728-Header-1024x576.jpg">
+        <v-row align="center">
         <v-content>
-          <v-container class ="fill-height" fluid>
+          <v-container fill-height>
             <v-layout align-center justify-center>
               <v-flex xs6>
                 <div class="text-xs-center">
@@ -26,15 +25,19 @@
             </v-layout>
           </v-container>
         </v-content>
-    <!-- </div> -->
+        </v-row>
+      </v-parallax>
+    </div>
     
+      <v-content>
+        <v-container class="fill-height" fluid>
+          <h1>You Are Not Alone</h1>
+        </v-container>
+      </v-content>
+
       <v-footer app>
         <span>&copy; TechTogether 2020</span>
       </v-footer>
-    </v-row>
-
-      </v-parallax>
-
     </v-app>
 </template>
 
---
+++ src/views/ishika.vue
@@ -0,0 +1,59 @@
+<template>
+
+  <v-app id="inspire">
+    <v-app-bar app clipped>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Help At Hand</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <!-- <v-btn icon :to="{ path: 'doctor' }">
+        <v-icon>mdi-doctor</v-icon>
+      </v-btn> -->
+    </v-app-bar>
+    <div id="app">
+      <v-content>
+        <v-container fill-height>
+          <v-layout align-center justify-center>
+            <v-flex xs6>
+              <div class="text-xs-center">
+                <v-btn x-large rounded type="danger" @click="toggleLable">Find Help</v-btn>
+              </div>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-content>
+    </div>
+      <v-content>
+        <v-container class="fill-height" fluid>
+          <h1>You Are Not Alone</h1>
+        </v-container>
+      </v-content>
+
+      <v-footer app>
+        <span>&copy; TechTogether 2020</span>
+      </v-footer>
+    </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    source: String
+  },
+
+  data: () => ({
+    drawer: null
+  }),
+
+  components: {
+    //
+  }
+};
+</script>
+
+<style scoped>
+#v-content{
+  background-image: url("https://saludalmaximo.com/wp-content/uploads/2019/08/11263-Sexual_Assault_Resource_Page-1296x728-Header-1024x576.jpg");
+  background-size: cover;
+  
+}
+</style>
