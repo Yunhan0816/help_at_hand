f757b5048a3cc4ed24ecfa0e10a17cb203ee3920
Sheila Jimenez Morejon <jimenezh@bu.edu>
2020-02-01 16:19:18 -0500

    added markers for 4 hospitals

--- src/components/gmap.vue
+++ src/components/gmap.vue
@@ -1,5 +1,7 @@
 <template>
+<v-container>
   <div class="googleMap"/>
+</v-container>
 </template>
 
 <script>
@@ -17,15 +19,24 @@ export default {
         }
         map.setCenter(results[0].geometry.location);
         map.fitBounds(results[0].geometry.viewport);
-    //     var  waqiMapOverlay  =  new  google.maps.ImageMapType({  
-    //         getTileUrl:  function(coord,  zoom)  {  
-    //                   return  'https://tiles.waqi.info/tiles/usepa-aqi/'  +  zoom  +  "/"  +  coord.x  +  "/"  +  coord.y  +  ".png?token=_TOKEN_ID_";  
-    //         },  
-    //         name:  "Air  Quality",  
-    //     });  
-  
-    //   map.overlayMapTypes.insertAt(0,waqiMapOverlay);
+    
+
       });
+      const location = [{lat: 42.3348, lng: -71.0733}, {lat: 42.362, lng: -71.06}, {lat:42.3361, lng: -71.1075}, {lat: 42.3397, lng: -71.1049}];
+      const marker1 = new google.maps.Marker({position: location[0], map: map});
+      marker1.location
+      const marker2 = new google.maps.Marker({position: location[1], map: map});
+      marker2.location
+      
+      const marker3 = new google.maps.Marker({position: location[2], map: map});
+      marker3.location
+      const marker4 = new google.maps.Marker({position: location[3], map: map});
+      marker4.location
+
+      
+      
+    //   console.log(marker)
+   
     } catch (error) {
       console.error(error);
     }
@@ -42,5 +53,10 @@ body {
 .googleMap {
   width: 100%;
   height: 100%;
+}
+.container{
+    height: 100vh;
+    
+    width: 100wh
 }
 </style>
