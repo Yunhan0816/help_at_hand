d846110d4e401e60f8f58c1aaaf137c51b5b3ce8
Ishika <jishika@bu.edu>
2020-02-01 15:35:02 -0500

    button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,5 @@
 <template>
+
   <v-app id="inspire">
     <v-app-bar app clipped>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
@@ -8,17 +9,29 @@
         <v-icon>mdi-doctor</v-icon>
       </v-btn> -->
     </v-app-bar>
+    <div id="app">
+      <v-content>
+        <v-container fill-height>
+          <v-layout align-center justify-center>
+            <v-flex xs6>
+              <div class="text-xs-center">
+                <v-btn x-large rounded type="danger" @click="toggleLable">Find Help</v-btn>
+              </div>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-content>
+    </div>
+      <v-content>
+        <v-container class="fill-height" fluid>
+          <h1>You Are Not Alone</h1>
+        </v-container>
+      </v-content>
 
-    <v-content>
-      <v-container class="fill-height" fluid>
-        <h1>You Are Not Alone</h1>
-      </v-container>
-    </v-content>
-
-    <v-footer app>
-      <span>&copy; TechTogether 2020</span>
-    </v-footer>
-  </v-app>
+      <v-footer app>
+        <span>&copy; TechTogether 2020</span>
+      </v-footer>
+    </v-app>
 </template>
 
 <script>
@@ -36,3 +49,11 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+#v-content{
+  background-image: url("https://saludalmaximo.com/wp-content/uploads/2019/08/11263-Sexual_Assault_Resource_Page-1296x728-Header-1024x576.jpg");
+  background-size: cover;
+  
+}
+</style>
