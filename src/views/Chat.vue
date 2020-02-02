a50e9b8fff2c9f0bfe0f227b68938cbdb7ce357d
Hannah Huang <yunhanh@bu.edu>
2020-02-02 09:31:10 -0500

    fix most stuff

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,17 +1,48 @@
+<!--
 <template>
   <v-app class="inspire">
-    <v-app-bar app clipped>
-      <v-toolbar-title>Help At Hand</v-toolbar-title>
-    </v-app-bar>
-
+        <v-app-bar app clipped>
+          <v-toolbar-title>Help At Hand</v-toolbar-title>
+        </v-app-bar>
+        
     <v-parallax dark src="@/assets/background.png" class="background" height="770">
-      <h1>We are here to help, let us know about your worries</h1>
-      <ChatBox msg="Welcome to Your Vue.js App" />
+              <h1>We are here to help, let us know about your worries</h1>
+<ChatBox msg="Welcome to Your Vue.js App"/>
+        
     </v-parallax>
 
-    <v-footer app>
-      <span>&copy; TechTogether 2020</span>
-    </v-footer>
+
+        <v-footer app>
+          <span>&copy; TechTogether 2020</span>
+        </v-footer>
+
+  </v-app>
+
+</template>
+-->
+<template>
+  <v-app class="inspire">
+    <v-parallax dark src="@/assets/background.png" class="background" height="770">
+      <v-row align="center" justify="center">
+        <v-app-bar app clipped>
+          <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+          <v-toolbar-title>Help At Hand</v-toolbar-title>
+          <router-link :to="{ name: 'report' }">
+            <v-btn rounded type="danger" @click="toggleLable" x-large="true">
+              <a id="buttontext">Go Back</a>
+            </v-btn>
+          </router-link>
+        </v-app-bar>
+
+        <v-content>
+          <h2>We are here to help, let us know about your worries</h2>
+          <ChatBox msg="Welcome to Your Vue.js App" />
+        </v-content>
+        <v-footer app>
+          <span>&copy; TechTogether 2020</span>
+        </v-footer>
+      </v-row>
+    </v-parallax>
   </v-app>
 </template>
 
@@ -27,12 +58,12 @@ export default {
 </script>
 
 <style>
+#buttontext {
+  color: teal;
+}
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
