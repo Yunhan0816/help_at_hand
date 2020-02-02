1b4a5161fb63ea60b928bde3c5aa86510dfc56d3
Hannah Huang <yunhanh@bu.edu>
2020-02-02 00:38:28 -0500

    fix assualt, chain all views together

--- src/views/Assault.vue
+++ src/views/Assault.vue
@@ -1,13 +1,28 @@
 <template>
+  <v-container>
+    <router-link :to="{ name: 'report' }">
+      <v-layout align-center justify-center>
+        <v-btn id="goback" rounded type="danger" align="center" @click="toggleLable" x-large="true">
+          <a id="buttontext">Go Back To Case Report</a>
+        </v-btn>
+      </v-layout>
+    </router-link>
+    <h2>Map of Sexual Assault Cases Nearby</h2>
     <assaultMap />
+  </v-container>
 </template>
 
 <script>
-import assaultMap from '../components/assaultMap'
+import assaultMap from "../components/assaultMap";
 export default {
-    name: 'Assault',
-    components: {
-        assaultMap
-    }
-}
+  name: "Assault",
+  components: {
+    assaultMap
+  }
+};
 </script>
+<style scoped>
+#buttontext {
+  color: teal;
+}
+</style>
--- src/views/Hospital.vue
+++ src/views/Hospital.vue
@@ -1,16 +1,34 @@
 <template>
-    <v-container>
-        <h1>Nearby Hospitals</h1>
-        <gmap/>
-    </v-container>
-    
+  <v-container>
+    <h2>Nearby Hospitals</h2>
+    <gmap />
+    <router-link :to="{ name: 'assault' }">
+      <v-layout align-center justify-center>
+        <v-btn
+          id="findassualt"
+          rounded
+          type="danger"
+          align="center"
+          @click="toggleLable"
+          x-large="true"
+        >
+          <a id="buttontext">Show Me Map of Sexual Assault Statistics</a>
+        </v-btn>
+      </v-layout>
+    </router-link>
+  </v-container>
 </template>
 <script>
-import gmap from '../components/gmap' 
+import gmap from "../components/gmap";
 export default {
-    name: 'Hospital',
-    components: {
-        gmap
-    }
-}
+  name: "Hospital",
+  components: {
+    gmap
+  }
+};
 </script>
+<style scoped>
+#buttontext {
+  color: teal;
+}
+</style>
